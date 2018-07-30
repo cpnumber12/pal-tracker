027c131074b36913161ea98d713705efa8306c70
Chris Petras <cpnumber12@gmail.com>
2018-07-30 09:55:12 -0600

    Lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping("/")
     public String SayHello() {
-        return "Hello";
+        return "hello";
     }
 }
